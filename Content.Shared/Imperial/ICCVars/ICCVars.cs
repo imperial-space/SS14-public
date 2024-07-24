7264c162e614f8dc50a3931e65e1aca622203498
mirton <mirton14@gmail.com>
2024-07-24 04:14:31 +0000

    update iccvars

--- Content.Shared/Imperial/ICCVars/ICCVars.cs
+++ Content.Shared/Imperial/ICCVars/ICCVars.cs
@@ -7,6 +7,18 @@ namespace Content.Shared.Imperial.ICCVar;
 // ReSharper disable once InconsistentNaming
 public sealed class ICCVars : CVars
 {
+    /// <summary>
+    /// Enables autovote for map and preset in lobby
+    /// </summary>
+    public static readonly CVarDef<bool>
+        VoteAutoStartInLobby = CVarDef.Create("vote.autostartinlobby", true, CVar.SERVERONLY);
+        
+    /// <summary>
+    /// Timer for end round
+    /// </summary>
+    public static readonly CVarDef<int>
+        GameEndRoundDuration = CVarDef.Create("game.endroundduration", 40, CVar.SERVERONLY);
+
     #region Sponsors
     public static readonly CVarDef<string> SponsorsApiUrl =
         CVarDef.Create("sponsor.api_url", "", CVar.SERVERONLY);
