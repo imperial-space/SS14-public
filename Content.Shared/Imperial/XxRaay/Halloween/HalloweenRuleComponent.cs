bd4ace541fd0cc727cfb4142f524790886cef9c8
XxRaay <Rimkagg43@yandex.ru>
2025-10-10 11:52:45 +0500

    Version 1
    (cherry picked from commit ab1ee820a6273bba6e417182eacfb1b6939f857a)

---
+++ Content.Shared/Imperial/XxRaay/Halloween/HalloweenRuleComponent.cs
@@ -0,0 +1,10 @@
+using Content.Shared.GameTicking.Components;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Imperial.XxRaay.Halloween
+{
+    public sealed partial class HalloweenRuleComponent : Component
+    {
+        public bool Active { get; set; }
+    }
+}
