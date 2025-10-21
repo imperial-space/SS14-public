53cc094c6fc646dc792486115720afc372a4aded
XxRaay <Rimkagg43@yandex.ru>
2025-10-21 19:37:11 +0500

    Рефакторинг + фиксы
    (cherry picked from commit 46e418fa133da0fe08508e9e3cdfcf9c5aebc1c4)

---
+++ Content.Server/Imperial/XxRaay/Halloween/HalloweenMobComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Server.Imperial.XxRaay.Halloween;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class HalloweenMobComponent : Component
+{
+    
+}
