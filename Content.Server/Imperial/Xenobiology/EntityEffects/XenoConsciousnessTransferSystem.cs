f060b619b73d4f4e1297e4986176601571acac65
Fredik21-git <Fredik21-git@users.noreply.github.com>
2026-04-11 21:57:27 +0300

    Fix xenobio consciousness transfer transform dependency

--- Content.Server/Imperial/Xenobiology/EntityEffects/XenoConsciousnessTransferSystem.cs
+++ Content.Server/Imperial/Xenobiology/EntityEffects/XenoConsciousnessTransferSystem.cs
@@ -20,6 +20,7 @@ public sealed partial class XenoConsciousnessTransferSystem
     [Dependency] private readonly MindSystem         _mind     = default!;
     [Dependency] private readonly MobStateSystem     _mobState = default!;
     [Dependency] private readonly PopupSystem        _popup    = default!;
+    [Dependency] private readonly SharedTransformSystem _xform = default!;
 
     protected override void Effect(Entity<MetaDataComponent> entity, ref EntityEffectEvent<XenoConsciousnessTransferEffect> args)
     {
