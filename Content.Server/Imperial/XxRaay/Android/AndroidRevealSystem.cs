f42fd4421a4bad5746b19202b41a5d59f07d14bb
Fredik21-git <Fredik21-git@users.noreply.github.com>
2026-04-12 09:06:00 +0300

    Remove unused android metadata dependency

--- Content.Server/Imperial/XxRaay/Android/AndroidRevealSystem.cs
+++ Content.Server/Imperial/XxRaay/Android/AndroidRevealSystem.cs
@@ -28,7 +28,6 @@ public sealed class AndroidRevealSystem : EntitySystem
     [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly MobStateSystem _mobState = default!;
     [Dependency] private readonly ActionBlockerSystem _actionBlocker = default!;
-    [Dependency] private readonly MetaDataSystem _metaData = default!;
 
     public override void Initialize()
     {
