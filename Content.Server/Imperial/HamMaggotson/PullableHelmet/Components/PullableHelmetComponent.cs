0308b2aacf4349e45cc504d5cf1455c0f724b324
Maggotson <maggotsonmaggot@gmail.com>
2026-06-22 17:55:52 +0300

    fix

--- Content.Server/Imperial/HamMaggotson/PullableHelmet/Components/PullableHelmetComponent.cs
+++ Content.Server/Imperial/HamMaggotson/PullableHelmet/Components/PullableHelmetComponent.cs
@@ -2,7 +2,6 @@ using Content.Shared.Storage;
 using Robust.Shared.Audio;
 using Robust.Shared.Prototypes;
 using Robust.Shared.GameStates;
-using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 using Content.Shared.Inventory;
 namespace Content.Server.Imperial.HamMaggotson.PullableHelmet.Components;
